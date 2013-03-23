8ee5854e481fd168fa0cd23deb4b155ada244d27
Andrius Ordojan <Andrius.Or@gmail.com>
2013-03-23 20:54:44 +0100

    Created a constructor for the Item class with preconditions

--- src/main/java/dk/cphbusiness/models/FinalTreasure.java
+++ src/main/java/dk/cphbusiness/models/FinalTreasure.java
@@ -2,4 +2,8 @@ package dk.cphbusiness.models;
 
 public class FinalTreasure extends Item {
 
+    public FinalTreasure(String name, int value) {
+        super(name, value);
+    }
+
 }
--- src/main/java/dk/cphbusiness/models/Item.java
+++ src/main/java/dk/cphbusiness/models/Item.java
@@ -1,6 +1,17 @@
 package dk.cphbusiness.models;
 
+import com.google.common.base.Preconditions;
+
 public class Item {
     private String name;
     private int value;
+    
+    public Item(String name, int value) {
+        Preconditions.checkArgument(name != null, "Item name may not be null");
+        Preconditions.checkArgument(!name.isEmpty(), "Item name may not be an empty string");
+        Preconditions.checkArgument(value > 0, "Item value was %s but expected nonnegative", value);
+        
+        this.name = name;
+        this.value = value;
+    }
 }
