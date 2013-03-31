aa39e8439118c96a3d2de645e676e8cb1030a61c
Paul <paul.frunza.hd@gmail.com>
2013-03-31 20:41:07 +0200

    Added translator

--- src/main/java/dk/cphbusiness/AppStart.java
+++ src/main/java/dk/cphbusiness/AppStart.java
@@ -1,10 +1,21 @@
 package dk.cphbusiness;
 
 import dk.cphbusiness.commands.Commands;
+import dk.cphbusiness.utils.Translator;
 
 public class AppStart {
 
     public static void main(String[] args) {
+    	String startingLanguage = new String("en");
+    	String startingCountry = new String("US");
+        
+    	if (args.length >= 2){
+	        startingLanguage = new String(args[0]);
+	        startingCountry = new String(args[1]);
+    	}
+    	
+    	Translator.changeLanguage(startingLanguage, startingCountry);
+    	
         Commands commands = new Commands();
         new GameRunner(commands).start();
     }
---
+++ src/main/java/dk/cphbusiness/utils/Translator.java
@@ -0,0 +1,38 @@
+package dk.cphbusiness.utils;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Translator {
+	private static String localeLanguage = "en";
+	private static String localeCountry = "US";
+	private static ResourceBundle messages;
+	
+	{
+		initializeBundle();
+	}
+
+	private static void initializeBundle() {
+		Locale locale = new Locale(localeLanguage, localeCountry);
+		messages = ResourceBundle.getBundle("dk.cphbusiness.utils.messages.translations", locale);
+	}
+	
+	public static String getLocaleLanguage() {
+		return localeLanguage;
+	}
+
+	public static String getLocaleCountry() {
+		return localeCountry;
+	}
+
+	public static void changeLanguage(String newLanguage, String newCountry){
+		localeCountry = newCountry;
+		localeLanguage = newLanguage;
+		
+		initializeBundle();
+	}
+	
+	public static String getMessage(String messageKey){
+		return messages.getString(messageKey);
+	}
+}
